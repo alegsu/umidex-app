4a4edf840feffbafca9f107e10d09466b8e84c8d
alegsu <alessandro@shinyup.it>
2026-07-31 12:30:31 +0200

    Fix build.gradle.kts Kotlin DSL syntax

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,12 +41,13 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.0"
+        kotlinCompilerExtensionVersion = "1.5.2"
     }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.activity:activity-ktx:1.8.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
